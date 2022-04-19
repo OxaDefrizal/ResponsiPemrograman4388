8861d42f664c7d54e0154bb493c9a7eeafbf4b82
OxaDefrizal <101616969+OxaDefrizal@users.noreply.github.com>
2022-04-19 12:27:29 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -25,15 +25,15 @@ namespace ResponsiPemrograman4388
             kar2.Nik = "21.22.4388";
             kar2.Nama = "Defrizal";
             kar2.GajiBulanan = -3000000;
+            
             if (kar1.GajiBulanan < 0) 
             {
-                kar1.GajiBulanan = 0;
+                kar1.GajiBulanan = 0; // validasi karawan 1 = 0
             }
             if (kar2.GajiBulanan < 0)
             {
-                kar2.GajiBulanan = 0;
-            }
-            
+                kar2.GajiBulanan = 0; // validasi karawan 2 = 0
+            } 
             Console.WriteLine(); // baris kosong
 
             //pemanggilan method
@@ -44,20 +44,18 @@ namespace ResponsiPemrograman4388
             
             if (kar1.GajiBulanan >= 0)
             {
-                kar1.GajiBulanan += kar1.GajiBulanan * 10 / 100;
+                kar1.GajiBulanan += kar1.GajiBulanan * 10 / 100; // proses menghitung bunga kar1
             }
             if (kar2.GajiBulanan >= 0)
             {
-                kar2.GajiBulanan += kar2.GajiBulanan * 10 / 100;
+                kar2.GajiBulanan += kar2.GajiBulanan * 10 / 100; // proses menghitung bunga kar2
             }
-            Console.WriteLine("\n");
+            Console.WriteLine("\n"); // baris kosong
             Console.WriteLine("SELAMAT MENDAPATKAN NAIK GAJI");
             Console.WriteLine("---------SEBESAR 10%--------");
             Console.WriteLine("1. {0} {1}\t{2}", kar1.Nik, kar1.Nama, kar1.GajiBulanan);
             Console.WriteLine("2. {0} {1}\t{2}", kar2.Nik, kar2.Nama, kar2.GajiBulanan);
-
-
-             Console.ReadKey();        
+            Console.ReadKey();        
         }
     }
 }
